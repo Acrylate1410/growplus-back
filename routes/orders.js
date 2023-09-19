b9388179151449750f92f6fc2765e95a2853f630
Acrylate1410 <quangnm1410@gmail.com>
2023-09-19 16:53:13 +0700

    sad

--- routes/orders.js
+++ routes/orders.js
@@ -21,7 +21,6 @@ router.get('/get_orders', async (req, res) => {
 
 router.post('/save_order', async (req, res) => {
     let fullDateOrder = new Date()
-    fullDateOrder = fullDateOrder.getTime() + (7 * 60 * 60 * 60)
     let dateOrder = fullDateOrder.getDate()
     let monthOrder = fullDateOrder.getMonth() + 1
     let hourOrder = fullDateOrder.getHours()
