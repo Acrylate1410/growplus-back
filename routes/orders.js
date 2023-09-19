adb15ae0e2b4825f02ab8766c368c99c57fd3fe3
Acrylate1410 <quangnm1410@gmail.com>
2023-09-19 16:08:46 +0700

    origin

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 const Model = require('../model/order');
 const cors = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://growplus.onrender.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, Content-Type");
     next();
